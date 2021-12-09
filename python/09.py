3ca2405d0aff07fa0e8d2e6853d495231c8c2cfd
Anders Andersson <anders.andersson@tretton37.com>
2021-12-09 10:17:58 +0100

    tidying up day 09

--- python/09.py
+++ python/09.py
@@ -11,54 +11,16 @@ with open("09.input", "r") as f:
 
 hmap = [list(row) for row in input]
 
-
 def find_neighbours(point):
-    wall = '-'
-    try:
-      if point[1] - 1 < 0:
-        above = wall
-      else:
-        above = (point[0], point[1] - 1)
-    except IndexError:
-      above = wall
-    
-    try:
-      if hmap[point[1]][point[0] + 1]:
-        right = (point[0] + 1, point[1])
-      else:
-        right = wall
-    except IndexError:
-      right = wall
-    
-    try:
-      if hmap[point[1] + 1][point[0]]:
-        below = (point[0], point[1] + 1)
-      else:
-        below = wall
-    except IndexError:
-      below = wall
-    
-    try:
-      if point[0] - 1 < 0:
-        left = wall
-      else:
-        left = (point[0] - 1, point[1])
-    except IndexError:
-      left = wall
-    return [above, right, below, left]
-
-def new_find_neighbours(point):
   dirs = [(0,-1), (1,0), (0,1), (-1,0)]
   neighbours = []
   for dir in dirs:
-    # print(dir, point[0] + dir[0], point[1] + dir[1])
     testpoint = (point[0] + dir[0], point[1] + dir[1])
     try:
-      if testpoint[0] > len(hmap) or testpoint[0] < 0 or testpoint[1] > len(hmap) or testpoint[1] < 0:
+      if testpoint[0] >= len(hmap[0]) or testpoint[0] < 0 or testpoint[1] > len(hmap) - 1 or testpoint[1] < 0:
         neighbours.append('-')
       else:
         neighbours.append(testpoint)
-      # v = hmap[point[1] + dir[0]][point[0] + dir[1]]
     except IndexError:
       neighbours.append('-')
   
@@ -80,23 +42,31 @@ def get_basin(start_point, found = []):
 
   return found
 
+def part1():
+  lowest = []
+  for y, row in enumerate(hmap):
+    for x, col in enumerate(row):
+      neighbours = find_neighbours((x, y))
 
+      if all([int(col) < int(hmap[n[1]][n[0]]) for n in neighbours if n != '-']):
+        lowest.append(int(get_value((x, y))) + 1)
+        # points = get_basin((x, y), [(x, y)])
+        # basin_lengths.append(len(points))
+  print(sum(lowest))
 
-lowest = []
-basin_lengths = []
-for y, row in enumerate(hmap):
-  for x, col in enumerate(row):
-    neighbours = find_neighbours((x, y))
-    if all([int(col) < int(hmap[n[1]][n[0]]) for n in neighbours if n != '-']):
-      points = get_basin((x, y), [(x, y)])
-      basin_lengths.append(len(points))
+def part2():
+  basin_lengths = []
+  for y, row in enumerate(hmap):
+    for x, col in enumerate(row):
+      neighbours = find_neighbours((x, y))
 
+      if all([int(col) < int(hmap[n[1]][n[0]]) for n in neighbours if n != '-']):
+        points = get_basin((x, y), [(x, y)])
+        basin_lengths.append(len(points))
+  basins = sorted(basin_lengths)
+  total = math.prod(basins[-3:])
+  return total
 
-# print(new_find_neighbours((99,99)))
-# print(find_neighbours((99,99)))
-
-
-basins = sorted(basin_lengths)
-total = math.prod(basins[-3:])
-print(total)
+part1()
+print(part2())
 
