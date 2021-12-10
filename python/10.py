43c1bebec5ec9b6f5f6eb36eb0f4c2386c2043a7
Anders Andersson <anders.andersson@tretton37.com>
2021-12-10 12:10:04 +0100

    glfn

--- python/10.py
+++ python/10.py
@@ -4,10 +4,9 @@ from functools import reduce
 with open("10.input", "r") as f:
   input = f.read().split('\n')
 
-pairs = {'{':'}','<':'>','(':')','[':']'}
-
 def parse_line(line):
   values = {')': 3,']': 57,'}': 1197,'>': 25137}
+  pairs = {'{':'}','<':'>','(':')','[':']'}
   opened = []
   for c in list(line):
     if c in pairs.keys():
@@ -17,19 +16,13 @@ def parse_line(line):
       if c != pairs[closing]:
         return values[c]
   return [pairs[c] for c in reversed(opened)]
-    
-# def sum_score(acc, curr):
-#   acc *= 5
-#   acc += curr
-#   return acc
-
+  
 def calculate_score(matches):
-  score_table = [')', ']', '}', '>']
-  return reduce(lambda acc, curr: acc * 5 + curr, [score_table.index(c) + 1 for c in matches], 0)
+  return reduce(lambda acc, curr: acc * 5 + curr, [[')', ']', '}', '>'].index(c) + 1 for c in matches], 0)
 
 def part2():
   scores = [calculate_score(parse_line(match)) for match in [line for line in input if isinstance(parse_line(line), list)]]
-  return sorted(scores)[int(len(scores) / 2)]
+  return sorted(scores)[len(scores) // 2]
 
 print('part1', sum([parse_line(line) for line in input if isinstance(parse_line(line), int)]))
 print('part2', part2())
