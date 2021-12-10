dc81a5bfdd0d868729f93993821a546ecd4f750d
Anders Andersson <anders.andersson@tretton37.com>
2021-12-10 12:04:03 +0100

    more golf

--- python/10.py
+++ python/10.py
@@ -1,43 +1,10 @@
 import math
 from functools import reduce
 
-input = """[({(<(())[]>[[{[]{<()<>>
-[(()[<>])]({[<{<<[]>>(
-{([(<{}[<>[]}>{[]{[(<()>
-(((({<>}<{<{<>}{[]{[]{}
-[[<[([]))<([[{}[[()]]]
-[{[{({}]{}}([{[{{{}}([]
-{<[[]]>}<{[{[{[]{()[[[]
-[<(<(<(<{}))><([]([]()
-<{([([[(<>()){}]>(<<{{
-<{([{{}}[<[[[<>{}]]]>[]]""".split('\n')
-
-# with open("10.input", "r") as f:
-#   input = f.read().split('\n')
+with open("10.input", "r") as f:
+  input = f.read().split('\n')
 
 pairs = {'{':'}','<':'>','(':')','[':']'}
-
-def get_invalid_chars(line):
-  values = {')': 3,']': 57,'}': 1197,'>': 25137}
-  opened = []
-  for c in list(line):
-    if c in pairs.keys():
-      opened.append(c)
-    if c in pairs.values():
-      closing = opened.pop()
-      if c != pairs[closing]:
-        return values[c]
-  return 0
-
-def get_matches(line):
-  opened = []
-  for c in list(line):
-    if c in pairs.keys():
-      opened.append(c)
-    if c in pairs.values():
-      opened.pop()
-  matches = [pairs[c] for c in reversed(opened)]
-  return matches
 
 def parse_line(line):
   values = {')': 3,']': 57,'}': 1197,'>': 25137}
@@ -51,22 +18,18 @@ def parse_line(line):
         return values[c]
   return [pairs[c] for c in reversed(opened)]
     
-
-def sum_score(acc, curr):
-  acc *= 5
-  acc += curr
-  return acc
+# def sum_score(acc, curr):
+#   acc *= 5
+#   acc += curr
+#   return acc
 
 def calculate_score(matches):
   score_table = [')', ']', '}', '>']
-  return reduce(sum_score, [score_table.index(c) + 1 for c in matches], 0)
-
-def part1():
-  return sum([parse_line(line) for line in input if isinstance(parse_line(line), int)])
+  return reduce(lambda acc, curr: acc * 5 + curr, [score_table.index(c) + 1 for c in matches], 0)
 
 def part2():
-  scores = [calculate_score(get_matches(match)) for match in [line for line in input if get_invalid_chars(line) == 0]]
+  scores = [calculate_score(parse_line(match)) for match in [line for line in input if isinstance(parse_line(line), list)]]
   return sorted(scores)[int(len(scores) / 2)]
 
-print(part1())
-# print(part2())
+print('part1', sum([parse_line(line) for line in input if isinstance(parse_line(line), int)]))
+print('part2', part2())
