f96a5db24b38c622c5294b2ca59fcab9ee570ff6
Anders Andersson <anders.andersson@tretton37.com>
2021-12-10 16:23:35 +0100

    more golf

--- python/10.py
+++ python/10.py
@@ -1,24 +1,19 @@
-import math
 from functools import reduce
 
 with open("10.input", "r") as f:
   input = f.read().split('\n')
 
-def parse_line(line):
-  values = {')': 3,']': 57,'}': 1197,'>': 25137}
-  pairs = {'{':'}','<':'>','(':')','[':']'}
+def parse_line(line, pairs = {'{':'}','<':'>','(':')','[':']'}, values = {')': 3,']': 57,'}': 1197,'>': 25137}):
   opened = []
   for c in list(line):
     if c in pairs.keys():
       opened.append(c)
-    if c in pairs.values():
-      closing = opened.pop()
-      if c != pairs[closing]:
+    elif c != pairs[opened.pop()]:
         return values[c]
   return [pairs[c] for c in reversed(opened)]
   
 def calculate_score(matches):
-  return reduce(lambda acc, curr: acc * 5 + curr, [[')', ']', '}', '>'].index(c) + 1 for c in matches], 0)
+  return reduce(lambda acc, curr: acc * 5 + curr, [list(')]}>').index(c)+1 for c in matches], 0)
 
 def part2():
   scores = [calculate_score(parse_line(match)) for match in [line for line in input if isinstance(parse_line(line), list)]]
