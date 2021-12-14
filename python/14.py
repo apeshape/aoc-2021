a7fb83ad146d4829861c7deb0dc64dfb3451b2a3
Anders Andersson <anders.andersson@tretton37.com>
2021-12-14 17:29:59 +0100

    add last letter to count

--- python/14.py
+++ python/14.py
@@ -1,124 +1,124 @@
-input = """NNCB
+# input = """NNCB
 
-CH -> B
-HH -> N
-CB -> H
-NH -> C
-HB -> C
-HC -> B
-HN -> C
-NN -> C
-BH -> H
-NC -> B
-NB -> B
-BN -> B
-BB -> N
-BC -> B
-CC -> N
-CN -> C"""
-
-# input = """PPFCHPFNCKOKOSBVCFPP
-
-# VC -> N
-# SC -> H
-# CK -> P
-# OK -> O
-# KV -> O
-# HS -> B
-# OH -> O
-# VN -> F
-# FS -> S
-# ON -> B
-# OS -> H
-# PC -> B
-# BP -> O
-# OO -> N
-# BF -> K
-# CN -> B
-# FK -> F
-# NP -> K
-# KK -> H
-# CB -> S
-# CV -> K
-# VS -> F
-# SF -> N
-# KB -> H
-# KN -> F
-# CP -> V
-# BO -> N
-# SS -> O
-# HF -> H
-# NN -> F
-# PP -> O
-# VP -> H
-# BB -> K
-# VB -> N
-# OF -> N
-# SH -> S
-# PO -> F
-# OC -> S
-# NS -> C
-# FH -> N
-# FP -> C
-# SO -> P
-# VK -> C
-# HP -> O
-# PV -> S
-# HN -> K
-# NB -> C
-# NV -> K
-# NK -> B
-# FN -> C
-# VV -> N
-# BN -> N
-# BH -> S
-# FO -> V
-# PK -> N
-# PS -> O
-# CO -> K
-# NO -> K
-# SV -> C
-# KO -> V
-# HC -> B
-# BC -> N
-# PB -> C
-# SK -> S
-# FV -> K
-# HO -> O
-# CF -> O
-# HB -> P
-# SP -> N
-# VH -> P
-# NC -> K
-# KC -> B
-# OV -> P
-# BK -> F
-# FB -> F
-# FF -> V
-# CS -> F
-# CC -> H
-# SB -> C
-# VO -> V
-# VF -> O
-# KP -> N
-# HV -> H
-# PF -> H
-# KH -> P
-# KS -> S
-# BS -> H
-# PH -> S
-# SN -> K
-# HK -> P
-# FC -> N
-# PN -> S
+# CH -> B
 # HH -> N
-# OB -> P
-# BV -> S
-# KF -> N
-# OP -> H
-# NF -> V
-# CH -> K
-# NH -> P"""
+# CB -> H
+# NH -> C
+# HB -> C
+# HC -> B
+# HN -> C
+# NN -> C
+# BH -> H
+# NC -> B
+# NB -> B
+# BN -> B
+# BB -> N
+# BC -> B
+# CC -> N
+# CN -> C"""
+
+input = """PPFCHPFNCKOKOSBVCFPP
+
+VC -> N
+SC -> H
+CK -> P
+OK -> O
+KV -> O
+HS -> B
+OH -> O
+VN -> F
+FS -> S
+ON -> B
+OS -> H
+PC -> B
+BP -> O
+OO -> N
+BF -> K
+CN -> B
+FK -> F
+NP -> K
+KK -> H
+CB -> S
+CV -> K
+VS -> F
+SF -> N
+KB -> H
+KN -> F
+CP -> V
+BO -> N
+SS -> O
+HF -> H
+NN -> F
+PP -> O
+VP -> H
+BB -> K
+VB -> N
+OF -> N
+SH -> S
+PO -> F
+OC -> S
+NS -> C
+FH -> N
+FP -> C
+SO -> P
+VK -> C
+HP -> O
+PV -> S
+HN -> K
+NB -> C
+NV -> K
+NK -> B
+FN -> C
+VV -> N
+BN -> N
+BH -> S
+FO -> V
+PK -> N
+PS -> O
+CO -> K
+NO -> K
+SV -> C
+KO -> V
+HC -> B
+BC -> N
+PB -> C
+SK -> S
+FV -> K
+HO -> O
+CF -> O
+HB -> P
+SP -> N
+VH -> P
+NC -> K
+KC -> B
+OV -> P
+BK -> F
+FB -> F
+FF -> V
+CS -> F
+CC -> H
+SB -> C
+VO -> V
+VF -> O
+KP -> N
+HV -> H
+PF -> H
+KH -> P
+KS -> S
+BS -> H
+PH -> S
+SN -> K
+HK -> P
+FC -> N
+PN -> S
+HH -> N
+OB -> P
+BV -> S
+KF -> N
+OP -> H
+NF -> V
+CH -> K
+NH -> P"""
 
 
 template, instructions_str = input.split('\n\n')
@@ -156,7 +156,7 @@ def get_paircount(prev_pc):
   return pc
 
 pc = pair_count.copy()
-for _ in range(40):
+for _ in range(10):
   pc = get_paircount(pc)
 
 def get_letter_count_in_pc(pc):
@@ -170,5 +170,6 @@ def get_letter_count_in_pc(pc):
 
 print_pc(pc)
 lc = get_letter_count_in_pc(pc)
+lc[template[-1]] += 1
 
 print(max(lc.values()) - min(lc.values()))
